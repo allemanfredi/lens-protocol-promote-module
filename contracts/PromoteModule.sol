a39a0a6b0c72433eeef3c6e804b0b4ee8e3cfcef
Alessandro Manfredi <alle.manfredi@gmail.com>
2022-08-02 08:43:23 +0200

    feat(contracts): adds inheritance from ModuleBase

--- contracts/PromoteModule.sol
+++ contracts/PromoteModule.sol
@@ -2,35 +2,33 @@
 pragma solidity 0.8.10;
 
 import {IReferenceModule} from "@aave/lens-protocol/contracts/interfaces/IReferenceModule.sol";
+import {ModuleBase} from "@aave/lens-protocol/contracts/core/modules/ModuleBase.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract PromoteModule is IReferenceModule {
+contract PromoteModule is ModuleBase, IReferenceModule {
     struct Reward {
         address token;
         uint256 amount;
         bool collected;
     }
 
-    address public immutable hub;
     mapping(uint256 => mapping(uint256 => Reward)) public rewards;
 
     event Promoted(uint256 indexed pubId, uint256 indexed profileId, uint256 indexed collectorProfileId, address token, uint256 amount);
 
     event RewardCollected(uint256 indexed pubId, uint256 indexed collectorProfileId, uint256 indexed profileId, address token, uint256 amount);
 
-    constructor(address _hub) {
-        hub = _hub;
-    }
+    constructor(address _hub) ModuleBase(_hub) {}
 
     function initializeReferenceModule(
         uint256 _profileId,
         uint256 _pubId,
         bytes calldata _data
-    ) external override returns (bytes memory) {
+    ) external override onlyHub returns (bytes memory) {
         // TODO: handles multiple redeemers with different amounts ecc ecc ...
         (address token, uint256 amount, uint256 collectorProfileId) = abi.decode(_data, (address, uint256, uint256));
-        IERC20(token).transferFrom(IERC721(hub).ownerOf(_profileId), address(this), amount);
+        IERC20(token).transferFrom(IERC721(HUB).ownerOf(_profileId), address(this), amount);
 
         rewards[collectorProfileId][_pubId] = Reward(token, amount, false);
         emit Promoted(_pubId, _profileId, collectorProfileId, token, amount);
@@ -49,13 +47,13 @@ contract PromoteModule is IReferenceModule {
         uint256 _profileIdPointed,
         uint256 _pubIdPointed,
         bytes calldata /*_data*/
-    ) external override {
+    ) external override onlyHub {
         Reward storage reward = rewards[_profileId][_pubIdPointed];
         if (reward.collected == false) {
             reward.collected = true;
             address token = reward.token;
             uint256 amount = reward.amount;
-            IERC20(token).transfer(IERC721(hub).ownerOf(_profileId), amount);
+            IERC20(token).transfer(IERC721(HUB).ownerOf(_profileId), amount);
             emit RewardCollected(_pubIdPointed, _profileId, _profileIdPointed, token, amount);
         }
     }
--- contracts/TimedPromoteModule.sol
+++ contracts/TimedPromoteModule.sol
@@ -2,10 +2,11 @@
 pragma solidity 0.8.10;
 
 import {IReferenceModule} from "@aave/lens-protocol/contracts/interfaces/IReferenceModule.sol";
+import {ModuleBase} from "@aave/lens-protocol/contracts/core/modules/ModuleBase.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract TimedPromoteModule is IReferenceModule {
+contract TimedPromoteModule is ModuleBase, IReferenceModule {
     struct Reward {
         address token;
         uint256 amount;
@@ -13,7 +14,6 @@ contract TimedPromoteModule is IReferenceModule {
         bool collected;
     }
 
-    address public immutable hub;
     mapping(uint256 => mapping(uint256 => Reward)) public rewards;
 
     event Promoted(
@@ -27,18 +27,16 @@ contract TimedPromoteModule is IReferenceModule {
 
     event RewardCollected(uint256 indexed pubId, uint256 indexed collectorProfileId, uint256 indexed profileId, address token, uint256 amount);
 
-    constructor(address _hub) {
-        hub = _hub;
-    }
+    constructor(address _hub) ModuleBase(_hub) {}
 
     function initializeReferenceModule(
         uint256 _profileId,
         uint256 _pubId,
         bytes calldata _data
-    ) external override returns (bytes memory) {
+    ) external override onlyHub returns (bytes memory) {
         // TODO: handles multiple redeemers with different amounts ecc ecc ...
         (address token, uint256 amount, uint256 collectorProfileId, uint64 endTimestamp) = abi.decode(_data, (address, uint256, uint256, uint64));
-        IERC20(token).transferFrom(IERC721(hub).ownerOf(_profileId), address(this), amount);
+        IERC20(token).transferFrom(IERC721(HUB).ownerOf(_profileId), address(this), amount);
 
         rewards[collectorProfileId][_pubId] = Reward(token, amount, endTimestamp, false);
         emit Promoted(_pubId, _profileId, collectorProfileId, token, amount, endTimestamp);
@@ -57,13 +55,13 @@ contract TimedPromoteModule is IReferenceModule {
         uint256 _profileIdPointed,
         uint256 _pubIdPointed,
         bytes calldata /*_data*/
-    ) external override {
+    ) external override onlyHub {
         Reward storage reward = rewards[_profileId][_pubIdPointed];
         if (block.timestamp <= reward.endTimestamp && reward.collected == false) {
             reward.collected = true;
             address token = reward.token;
             uint256 amount = reward.amount;
-            IERC20(token).transfer(IERC721(hub).ownerOf(_profileId), amount);
+            IERC20(token).transfer(IERC721(HUB).ownerOf(_profileId), amount);
             emit RewardCollected(_pubIdPointed, _profileId, _profileIdPointed, token, amount);
         }
     }
